0393f55f23792c26942d19d4100a758d39afe7dc
ryantiarno <m.riantyarno@gmail.com>
2023-06-14 12:39:27 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Lupasandi from "./pages/Lupasandi";
-import Register from "./pages/Register";
-import Otp from "./pages/Otp";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Lupasandi from "./pages/Lupasandi";
+// import Register from "./pages/Register";
+// import Otp from "./pages/Otp";
+import Otp from "./pages/FlightForm";
+import FlightForm from "./pages/FlightForm";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Login" element={<Login />} />
+        <Route path="/" element={<FlightForm />} />
+          {/* <Route path="/Home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/Lupasandi" element={<Lupasandi />} />
           <Route path="/Register" element={<Register />} />
-          <Route path="/verifikasi-otp" element={<Otp />} />
+          <Route path="/verifikasi-otp" element={<Otp />} /> */}
         </Routes>
       </BrowserRouter>
     </>

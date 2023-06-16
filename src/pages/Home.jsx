a7862b9384f8ec38d16f5e8ea5f2c6396d9c5f90
Latifano <ikromilatifano76@gmail.com>
2023-06-16 12:38:10 +0700

    benerin Link + card destination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 import { Card } from "react-bootstrap";
 
@@ -316,6 +317,62 @@ function Home() {
             {/* Card Fav Destination */}
             <div className="card-destination">
               <Card className="card-kategori">
+                <Card.Img
+                  className="img_kategori"
+                  variant="top"
+                  src={img_destination1}
+                />
+                <Card.Body>
+                  <h6 className="destination">Jakarta - Bangkok</h6>
+                  <h6 className="airline">AirAsia</h6>
+                  <h6 className="date">20 - 30 Maret 2023</h6>
+                  <h6 className="price">Mulai dari IDR 950.000 </h6>
+                </Card.Body>
+              </Card>
+
+              <Card className="card-kategori2">
+                <Card.Img
+                  className="img_kategori"
+                  variant="top"
+                  src={img_destination1}
+                />
+                <Card.Body>
+                  <h6 className="destination">Jakarta - Bangkok</h6>
+                  <h6 className="airline">AirAsia</h6>
+                  <h6 className="date">20 - 30 Maret 2023</h6>
+                  <h6 className="price">Mulai dari IDR 950.000 </h6>
+                </Card.Body>
+              </Card>
+
+              <Card className="card-kategori3">
+                <Card.Img
+                  className="img_kategori"
+                  variant="top"
+                  src={img_destination1}
+                />
+                <Card.Body>
+                  <h6 className="destination">Jakarta - Bangkok</h6>
+                  <h6 className="airline">AirAsia</h6>
+                  <h6 className="date">20 - 30 Maret 2023</h6>
+                  <h6 className="price">Mulai dari IDR 950.000 </h6>
+                </Card.Body>
+              </Card>
+
+              <Card className="card-kategori4">
+                <Card.Img
+                  className="img_kategori"
+                  variant="top"
+                  src={img_destination1}
+                />
+                <Card.Body>
+                  <h6 className="destination">Jakarta - Bangkok</h6>
+                  <h6 className="airline">AirAsia</h6>
+                  <h6 className="date">20 - 30 Maret 2023</h6>
+                  <h6 className="price">Mulai dari IDR 950.000 </h6>
+                </Card.Body>
+              </Card>
+
+              <Card className="card-kategori5">
                 <Card.Img
                   className="img_kategori"
                   variant="top"

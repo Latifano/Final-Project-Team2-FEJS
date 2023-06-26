0cf8cb035dd71c4a68cd6c13d86e1eb46a5cd5d4
Latifano <ikromilatifano76@gmail.com>
2023-06-26 12:14:03 +0700

    styling accordion

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import HeaderLogin from "../components/HeaderLogin";
@@ -9,6 +9,7 @@ import arrow_up_down from "../styles/images/arrow_up_down.png";
 import transit_icon from "../styles/images/fi_box.png";
 import fasilitas_icon from "../styles/images/fi_heart.png";
 import harga_icon from "../styles/images/fi_dollar-sign.png";
+import arrow_accordion from "../styles/images/arrow_accordion.png";
 
 import Button from "react-bootstrap/Button";
 import Loading from "../components/search-result/Loading";
@@ -151,25 +152,80 @@ function SearchResult(props) {
           {/* <Empty /> */}
           {/* Accordion Result */}
           <div className="container_accordion">
-            {data.map((data) => (
-              <Accordion className="box_accordion">
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <Typography>
-                    {data.airline.name} - {data.info.seat_class}
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
-                    ea recusandae minus, nulla corrupti nam!
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
-            ))}
+            <Accordion className="box_accordion">
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>
+                  <div className="mt-3">
+                    <div className="issued d-flex align-items-center justify-content-between">
+                      <h5 className="fw-bold">Jet Air - Economy</h5>
+                    </div>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <h6 className="fw-bold me-3">19:10</h6>
+                      <img
+                        src={arrow_accordion}
+                        alt="icon_arrow_accordion"
+                        className="me-3"
+                      />
+                      <h6 className="fw-bold me-5">21:00</h6>
+                      <h6 className="text-color">IDR 4.950.000</h6>
+                    </div>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <h6 className="fw-bold me-4">JKT</h6>
+                      <p className="ms-2 me-2">1h 50m</p>
+                      <h6 className="fw-bold ms-4">MLB</h6>
+                      <Button
+                        type="submit"
+                        className="custom-button-lgn text-light w-10"
+                      >
+                        Pesan
+                      </Button>
+                    </div>
+                  </div>
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  <div className="mt-3">
+                    <div className="issued d-flex align-items-center justify-content-between">
+                      <h5 className="fw-bold">Detail Pesanan</h5>
+                    </div>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <h6 className="fw-bold">19:10</h6>
+                      <h6 className="text-color">Keberangkatan</h6>
+                    </div>
+                    <p className="mb-0">5 Maret 2023</p>
+                    <p>Soekarno Hatta - Terminal 1A Domestik</p>
+                  </div>
+
+                  <hr />
+                  <Col xs={1} className="d-flex align-items-center">
+                    <Col md="auto">
+                      <h6 className="fw-bold">Jet Air - Economy</h6>
+                      <h6 className="fw-bold mb-4">JT - 203</h6>
+                      <h6 className="fw-bold">Informasi:</h6>
+                      <p>Baggage</p>
+                      <p>Cabin Baggage</p>
+                      <p>In Flight Entertainment</p>
+                    </Col>
+                  </Col>
+
+                  <hr />
+
+                  <div className="div">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <h6>21:10</h6>
+                      <h6 className="text-color">Kedatangan</h6>
+                    </div>
+                    <p className="mb-0">5 Maret 2023</p>
+                    <p className="fw-bold">Melbourne International Airport</p>
+                  </div>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
           </div>
         </Row>
       </Container>
